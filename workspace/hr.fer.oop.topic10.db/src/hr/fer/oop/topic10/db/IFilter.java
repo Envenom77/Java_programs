077e08c4f3983dd103a95802c4c30bbd2e40b71c
Josip Gatjal <prangija77@gmail.com>
2015-07-11 22:54:41 +0200

    bla

--- workspace/hr.fer.oop.topic10.db/src/hr/fer/oop/topic10/db/IFilter.java
+++
@@ -1,7 +0,0 @@
-package hr.fer.oop.topic10.db;
-
-public interface IFilter {
-	
-	public boolean accepts(StudentRecord record);
-
-}
--- workspace/hr.fer.oop.week9.db/src/hr/fer/oop/week9/db/IFilter.java
+++
@@ -1,7 +0,0 @@
-package hr.fer.oop.week9.db;
-
-public interface IFilter {
-	
-	public boolean accepts(StudentRecord record);
-
-}
